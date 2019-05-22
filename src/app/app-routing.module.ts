e19ccbdd767bd70642149e0e7ac7069adea15653
KrzysiekJodlowski <krzysztof.s.jodlowski@gmail.com>
2019-05-22 11:11:34 +0200

    Implement component routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { ContactComponent } from "./contact/contact.component";
+import { GalleryComponent } from "./gallery/gallery.component";
+import { HomeComponent } from "./home/home.component";
+import { NewsComponent } from "./news/news.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "", component: HomeComponent },
+  { path: "news", component: NewsComponent },
+  { path: "gallery", component: GalleryComponent },
+  { path: "contact", component: ContactComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
