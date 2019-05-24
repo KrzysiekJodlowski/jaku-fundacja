15c2abf08591d61083da4afda9a873ce4780e451
KrzysiekJodlowski <krzysztof.s.jodlowski@gmail.com>
2019-05-24 16:59:13 +0200

    Implement news and news titles download from Firebase

--- src/app/news/news.component.ts
+++ src/app/news/news.component.ts
@@ -1,15 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { AngularFireDatabase } from "@angular/fire/database";
+import { loadInternal } from "@angular/core/src/render3/util";
 
 @Component({
-  selector: 'app-news',
-  templateUrl: './news.component.html',
-  styleUrls: ['./news.component.css']
+  selector: "app-news",
+  templateUrl: "./news.component.html",
+  styleUrls: ["./news.component.css"]
 })
 export class NewsComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+  private news: any[];
+  private dbTitles: any[];
+  private titles: string[];
+  constructor(private fireDb: AngularFireDatabase) {
+    this.titles = [];
+    this.dbTitles = [];
   }
 
+  ngOnInit() {
+    this.fireDb
+      .list("/news")
+      .snapshotChanges()
+      .subscribe(dbTitles => {
+        this.dbTitles = dbTitles;
+        this.dbTitles.forEach(title => {
+          this.titles.push(title.key);
+        });
+        console.log(this.titles);
+      });
+    this.fireDb
+      .list("/news")
+      .valueChanges()
+      .subscribe(news => {
+        this.news = news;
+        console.log(this.news);
+      });
+  }
 }
