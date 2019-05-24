592dbc3ada9c37d2842f8cb151200cc7d51c9075
KrzysiekJodlowski <krzysztof.s.jodlowski@gmail.com>
2019-05-24 13:40:44 +0200

    Install and set Firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,25 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { AngularFireModule } from "@angular/fire";
+import { AngularFireDatabaseModule } from "@angular/fire/database";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { NavbarComponent } from './navbar/navbar.component';
-import { FooterComponent } from './footer/footer.component';
-import { HomeComponent } from './home/home.component';
-import { NewsComponent } from './news/news.component';
-import { GalleryComponent } from './gallery/gallery.component';
-import { ContactComponent } from './contact/contact.component';
-import { AboutComponent } from './about/about.component';
-import { PeopleComponent } from './people/people.component';
-import { EventComponent } from './event/event.component';
-import { SubgalleryComponent } from './subgallery/subgallery.component';
-import { ContactformComponent } from './contactform/contactform.component';
-import { NewspreviewComponent } from './newspreview/newspreview.component';
-import { AboutpreviewComponent } from './aboutpreview/aboutpreview.component';
-import { GallerypreviewComponent } from './gallerypreview/gallerypreview.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { NavbarComponent } from "./navbar/navbar.component";
+import { FooterComponent } from "./footer/footer.component";
+import { HomeComponent } from "./home/home.component";
+import { NewsComponent } from "./news/news.component";
+import { GalleryComponent } from "./gallery/gallery.component";
+import { ContactComponent } from "./contact/contact.component";
+import { AboutComponent } from "./about/about.component";
+import { PeopleComponent } from "./people/people.component";
+import { EventComponent } from "./event/event.component";
+import { SubgalleryComponent } from "./subgallery/subgallery.component";
+import { ContactformComponent } from "./contactform/contactform.component";
+import { NewspreviewComponent } from "./newspreview/newspreview.component";
+import { AboutpreviewComponent } from "./aboutpreview/aboutpreview.component";
+import { GallerypreviewComponent } from "./gallerypreview/gallerypreview.component";
+import { environment } from "src/environments/environment";
 
 @NgModule({
   declarations: [
@@ -38,9 +41,11 @@ import { GallerypreviewComponent } from './gallerypreview/gallerypreview.compone
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireDatabaseModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
