67a593258703ec30bb4136a3af596d0ff341b9e4
KrzysiekJodlowski <krzysztof.s.jodlowski@gmail.com>
2019-05-28 10:42:21 +0200

    Implement basic news loading Remove unecessary console logs

--- src/app/news/news.component.ts
+++ src/app/news/news.component.ts
@@ -25,14 +25,12 @@ export class NewsComponent implements OnInit {
         this.dbTitles.forEach(title => {
           this.titles.push(title.key);
         });
-        console.log(this.titles);
       });
     this.fireDb
       .list("/news")
       .valueChanges()
       .subscribe(news => {
         this.news = news;
-        console.log(this.news);
       });
   }
 }
