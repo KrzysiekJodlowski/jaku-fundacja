2186d6a73f5e73766ff5dad6d842a0ce6183528e
KrzysiekJodlowski <krzysztof.s.jodlowski@gmail.com>
2019-05-29 14:07:02 +0200

    Implement WebstorageService

--- src/app/services/webstorage/webstorage.service.ts
+++ src/app/services/webstorage/webstorage.service.ts
@@ -1,9 +1,17 @@
-import { Injectable } from '@angular/core';
+import { Injectable, Inject } from "@angular/core";
+import { SESSION_STORAGE, StorageService } from "ngx-webstorage-service";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root"
 })
 export class WebstorageService {
+  constructor(@Inject(SESSION_STORAGE) private storage: StorageService) {}
 
-  constructor() { }
+  public loadDataToStorage(storageKey: string, data: any) {
+    this.storage.set(storageKey, data);
+  }
+
+  public loadDataFromStorage(storageKey: string): any {
+    return this.storage.get(storageKey);
+  }
 }
