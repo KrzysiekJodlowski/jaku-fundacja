2a07bfa642b7875af131f619b5a3a2e4509dcc30
KrzysiekJodlowski <krzysztof.s.jodlowski@gmail.com>
2019-06-05 12:16:25 +0200

    Import EmailJS library and implement sendMail method in ContactformComponent

--- src/app/contactform/contactform.component.ts
+++ src/app/contactform/contactform.component.ts
@@ -1,15 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import * as emailjs from "emailjs-com";
 
 @Component({
-  selector: 'app-contactform',
-  templateUrl: './contactform.component.html',
-  styleUrls: ['./contactform.component.css']
+  selector: "app-contactform",
+  templateUrl: "./contactform.component.html",
+  styleUrls: ["./contactform.component.css"]
 })
 export class ContactformComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  sendMail() {
+    const templateParams = {
+      from_name: "Jasio",
+      message_html: "Jasio"
+    };
+
+    emailjs
+      .send(
+        "gmail",
+        "template_gqZQnCzA",
+        templateParams,
+        "user_ZfJq9Et9cpKuBEA8cjwr9"
+      )
+      .then(response => {
+        console.log("SUCCESS!", response.status, response.text);
+      })
+      .catch(error => {
+        console.log(error);
+      });
   }
-
 }
