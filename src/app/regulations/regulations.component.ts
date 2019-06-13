d7fd875bcd9665b60a38c53280d3fb7d97ba6e61
KrzysiekJodlowski <krzysztof.s.jodlowski@gmail.com>
2019-06-13 11:16:56 +0200

    Minimize RegulationsComponent implementation

--- src/app/regulations/regulations.component.ts
+++ src/app/regulations/regulations.component.ts
@@ -1,15 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-regulations',
-  templateUrl: './regulations.component.html',
-  styleUrls: ['./regulations.component.scss']
+  selector: "app-regulations",
+  templateUrl: "./regulations.component.html",
+  styleUrls: ["./regulations.component.scss"]
 })
-export class RegulationsComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
-}
+export class RegulationsComponent {}
