97bf88eb3a9f81b80aba4d519d64f4fa255ebc0e
Dariusz Domański <darski@XiaomiMiAir13.3>
2019-06-13 12:43:03 +0200

    Import AlertModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 import { CarouselModule } from "ngx-bootstrap/carousel";
 import { BsDropdownModule } from "ngx-bootstrap/dropdown";
+import { AlertModule } from "ngx-bootstrap/alert";
 
 // imports below will be used in admin panel
 import { AngularFireModule } from "@angular/fire";
@@ -59,7 +60,8 @@ import { RegulationsComponent } from "./regulations/regulations.component";
     FormsModule,
     HttpClientModule,
     CarouselModule.forRoot(),
-    BsDropdownModule.forRoot()
+    BsDropdownModule.forRoot(),
+    AlertModule.forRoot()
   ],
   providers: [TimeService, NewsService],
   bootstrap: [AppComponent]
