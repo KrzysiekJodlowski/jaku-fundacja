73402aa6aef505551c89856cea9c3de69bda3feb
KrzysiekJodlowski <krzysztof.s.jodlowski@gmail.com>
2019-06-14 12:45:15 +0200

    Implement pageChanged method in PageChangedService

--- src/app/services/page_changed/page-changed.service.ts
+++ src/app/services/page_changed/page-changed.service.ts
@@ -1,9 +1,17 @@
-import { Injectable } from '@angular/core';
+import { Injectable } from "@angular/core";
+import { PageChangedEvent } from "ngx-bootstrap/pagination";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root"
 })
 export class PageChangedService {
-
-  constructor() { }
+  public pageChanged(
+    event: PageChangedEvent,
+    items: Object[],
+    currentItems: Object[]
+  ): void {
+    const startItem = (event.page - 1) * event.itemsPerPage;
+    const endItem = event.page * event.itemsPerPage;
+    currentItems = items.slice(startItem, endItem);
+  }
 }
