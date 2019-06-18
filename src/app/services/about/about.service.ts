9e4b333dbc429913a7d7f9b5f55f2b470ed55f53
KrzysiekJodlowski <krzysztof.s.jodlowski@gmail.com>
2019-06-18 11:44:10 +0200

    Implement getAboutObjectsFromDb function in AboutService

--- src/app/services/about/about.service.ts
+++ src/app/services/about/about.service.ts
@@ -1,9 +1,17 @@
-import { Injectable } from '@angular/core';
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root"
 })
 export class AboutService {
+  constructor(private http: HttpClient) {}
 
-  constructor() { }
+  public getAboutObjectsFromDb() {
+    return new Promise((resolve, reject) => {
+      this.http
+        .get("https://us-central1-fir-7d0a4.cloudfunctions.net/getAbout")
+        .subscribe(about => resolve(about));
+    });
+  }
 }
