05ebea5ab38aaf9ea3837976f6ad0eacae9c43e4
Dariusz Domański <darski@XiaomiMiAir13.3>
2019-06-20 15:39:34 +0200

    Create login component

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,15 +1,45 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { AuthServiceService } from "../services/auth-service/auth-service.service";
+import {
+  FormGroup,
+  FormControl,
+  Validators,
+  FormBuilder
+} from "@angular/forms";
+import { Router } from "@angular/router";
 
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  selector: "app-login",
+  templateUrl: "./login.component.html",
+  styleUrls: ["./login.component.scss"]
 })
 export class LoginComponent implements OnInit {
+  loginForm: FormGroup;
+  errorMessage: string = "";
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(
+    private authService: AuthServiceService,
+    private formBuilder: FormBuilder,
+    private router: Router
+  ) {
+    this.loginForm = this.formBuilder.group({
+      password: ["", Validators.required],
+      email: ["", Validators.required]
+    });
   }
 
+  ngOnInit() {}
+
+  tryLogin(value) {
+    console.log(value);
+    this.authService.doEmailLogin(value).then(
+      res => {
+        this.router.navigate(["admin/admin_panel"]);
+      },
+      err => {
+        console.log(err);
+        this.errorMessage = err.errorMessage;
+      }
+    );
+  }
 }
