b3862e3d007435b98784ce84c4df295dafd189c4
Dariusz Domański <darski@XiaomiMiAir13.3>
2019-06-21 14:01:04 +0200

    Create logOut method

--- src/app/admin-panel/admin-home/admin-navbar/admin-navbar.component.ts
+++ src/app/admin-panel/admin-home/admin-navbar/admin-navbar.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { AuthServiceService } from "src/app/services/auth-service/auth-service.service";
+import { Router } from "@angular/router";
 
 @Component({
-  selector: 'app-admin-navbar',
-  templateUrl: './admin-navbar.component.html',
-  styleUrls: ['./admin-navbar.component.scss']
+  selector: "app-admin-navbar",
+  templateUrl: "./admin-navbar.component.html",
+  styleUrls: ["./admin-navbar.component.scss"]
 })
 export class AdminNavbarComponent implements OnInit {
+  constructor(
+    private authService: AuthServiceService,
+    private router: Router
+  ) {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  public logOut() {
+    this.authService.logOut();
+    this.router.navigate(["admin/login"]);
   }
-
 }
