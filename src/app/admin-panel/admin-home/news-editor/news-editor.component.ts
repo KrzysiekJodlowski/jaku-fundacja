989a9f1fb5f8dce409156a1962fa5564c3e8f926
KrzysiekJodlowski <krzysztof.s.jodlowski@gmail.com>
2019-06-24 13:59:25 +0200

    Inject NewsService and TimeService into NewsEditor Implement simple news listing in NewsEditor

--- src/app/admin-panel/admin-home/news-editor/news-editor.component.ts
+++ src/app/admin-panel/admin-home/news-editor/news-editor.component.ts
@@ -1,15 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { NewsService } from "../../../services/news/news.service";
+import { TimeService } from "../../../services/time/time.service";
 
 @Component({
-  selector: 'app-news-editor',
-  templateUrl: './news-editor.component.html',
-  styleUrls: ['./news-editor.component.scss']
+  selector: "app-news-editor",
+  templateUrl: "./news-editor.component.html",
+  styleUrls: ["./news-editor.component.scss"]
 })
 export class NewsEditorComponent implements OnInit {
+  private news: Object[];
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(
+    private newsService: NewsService,
+    private timeService: TimeService
+  ) {
+    this.news = [];
   }
 
+  ngOnInit() {
+    this.newsService.getNewsObjectsFromDb().then(news => {
+      Object.values(news).forEach(info => {
+        this.addInfo(info);
+      });
+    });
+  }
+
+  private addInfo(info: any) {
+    this.news.push({
+      date: this.timeService.formatDate(info.date),
+      title: info.title,
+      content: info.content
+    });
+  }
 }
