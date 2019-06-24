e576ed502db77c721093632e976243e2bb7395e1
Dariusz Domański <darski@XiaomiMiAir13.3>
2019-06-24 15:32:16 +0200

    Create logic for obtaining pictures from firebase

--- src/app/admin-panel/admin-home/gallery-editor/gallery-editor.component.ts
+++ src/app/admin-panel/admin-home/gallery-editor/gallery-editor.component.ts
@@ -1,15 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { GalleryService } from "src/app/services/gallery/gallery.service";
 
 @Component({
-  selector: 'app-gallery-editor',
-  templateUrl: './gallery-editor.component.html',
-  styleUrls: ['./gallery-editor.component.scss']
+  selector: "app-gallery-editor",
+  templateUrl: "./gallery-editor.component.html",
+  styleUrls: ["./gallery-editor.component.scss"]
 })
 export class GalleryEditorComponent implements OnInit {
+  private galleries: Object[];
+  private currentGalleries: Object[];
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private galleryService: GalleryService) {
+    this.galleries = [];
   }
 
+  ngOnInit() {
+    this.obtainImagesObjectsFromDatabase();
+  }
+
+  private obtainImagesObjectsFromDatabase() {
+    this.galleryService.getImageObjectsFromDb().then(galleries => {
+      Object.entries(galleries).forEach(gallery => {
+        this.galleries.push(gallery);
+      });
+      this.currentGalleries = this.galleries.slice(0, 2);
+    });
+  }
 }
