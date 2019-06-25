bd91cf778a04b0f9ff8b39ed92e7b119ae4c37de
KrzysiekJodlowski <krzysztof.s.jodlowski@gmail.com>
2019-06-25 12:16:16 +0200

    Add AngularFireDatabase object into NewsService Implement removeNewsFromDb function

--- src/app/services/news/news.service.ts
+++ src/app/services/news/news.service.ts
@@ -1,11 +1,15 @@
 import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
+import { AngularFireDatabase } from "@angular/fire/database";
 
 @Injectable({
   providedIn: "root"
 })
 export class NewsService {
-  constructor(private http: HttpClient) {}
+  constructor(
+    private http: HttpClient,
+    private dataBase: AngularFireDatabase
+  ) {}
 
   public getNewsObjectsFromDb() {
     return new Promise((resolve, reject) => {
@@ -13,5 +17,10 @@ export class NewsService {
         .get("https://us-central1-fir-7d0a4.cloudfunctions.net/getNews")
         .subscribe(news => resolve(news));
     });
+  }
+
+  public removeNewsFromDb(newsTag: string) {
+    const newsReference = this.dataBase.object(`news/${newsTag}`);
+    newsReference.remove();
   }
 }
