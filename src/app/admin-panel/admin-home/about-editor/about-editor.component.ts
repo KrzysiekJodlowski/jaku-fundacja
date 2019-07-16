f83e2f12a8829483b21999951f0a39f5e779c537
KrzysiekJodlowski <krzysztof.s.jodlowski@gmail.com>
2019-07-16 09:49:08 +0200

    Initialize about-editor subcomponents

--- src/app/admin-panel/admin-home/about-editor/about-editor.component.ts
+++ src/app/admin-panel/admin-home/about-editor/about-editor.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { AboutService } from "../../../services/about/about.service";
 
 @Component({
-  selector: 'app-about-editor',
-  templateUrl: './about-editor.component.html',
-  styleUrls: ['./about-editor.component.scss']
+  selector: "app-about-editor",
+  templateUrl: "./about-editor.component.html",
+  styleUrls: ["./about-editor.component.scss"]
 })
 export class AboutEditorComponent implements OnInit {
+  private about: Object[];
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private aboutService: AboutService) {
+    this.about = [];
   }
 
+  ngOnInit() {
+    this.aboutService.getAboutObjectsFromDb().then(aboutUs => {
+      Object.values(aboutUs).forEach(person => {
+        this.about.push(person);
+      });
+    });
+  }
 }
---
+++ src/app/admin-panel/admin-home/about-editor/delete-window/delete-window.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-delete-window',
+  templateUrl: './delete-window.component.html',
+  styleUrls: ['./delete-window.component.scss']
+})
+export class DeleteWindowComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
