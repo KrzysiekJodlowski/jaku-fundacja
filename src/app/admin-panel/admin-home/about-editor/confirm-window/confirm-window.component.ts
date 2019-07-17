39a87b9575175bf46d5887f6659c3546ca0bc907
dardomanski <dardomanski@gmail.com>
2019-07-17 13:56:19 +0200

    Implement ConfirmWindowComponent

--- src/app/admin-panel/admin-home/about-editor/confirm-window/confirm-window.component.ts
+++ src/app/admin-panel/admin-home/about-editor/confirm-window/confirm-window.component.ts
@@ -1,12 +1,31 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, Input, ViewChild, ElementRef } from "@angular/core";
+import { BsModalService, BsModalRef } from "ngx-bootstrap/modal";
+import { $ } from "protractor";
 
 @Component({
   selector: "about-confirm-window",
   templateUrl: "./confirm-window.component.html",
   styleUrls: ["./confirm-window.component.scss"]
 })
-export class ConfirmWindowComponent implements OnInit {
-  constructor() {}
+export class ConfirmWindowComponent {
+  private confirmWindowStatement: string;
+  @ViewChild("template") template: ElementRef;
+  @ViewChild("alert") alert: ElementRef;
+  private success: boolean = false;
 
-  ngOnInit() {}
+  modalRef: BsModalRef;
+  config = {
+    animated: true
+  };
+
+  constructor(private modalService: BsModalService) {}
+
+  public show(confirmWindowStatement: string, isOperationSucceed: boolean) {
+    this.confirmWindowStatement = confirmWindowStatement;
+    this.success = isOperationSucceed;
+    this.modalRef = this.modalService.show(this.template, this.config);
+    setTimeout(() => {
+      this.modalRef.hide();
+    }, 4000);
+  }
 }
